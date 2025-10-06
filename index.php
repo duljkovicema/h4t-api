f39ca0678da3f5f9e0919870108d6f7f25aa51d9
unknown <duljkovicema@gmail.com>
2025-10-06 10:05:47 +0200

    Revolut-webhook

--- index.php
+++ index.php
@@ -113,14 +113,6 @@ if (preg_match("/\/upload-tree$/", $request)) {
         http_response_code(405);
         echo json_encode(["error" => "Method not allowed"]);
     }
-} } elseif (preg_match("/\/revolut-webhook$/", $request)) {
-    if ($method === 'POST') {
-        require_once 'revolut-webhook.php';
-        handleRevolutWebhook($pdo);
-    } else {
-        http_response_code(405);
-        echo json_encode(["error" => "Method not allowed"]);
-    }
 } else {
     http_response_code(404);
     echo json_encode(["request_uri" => $_SERVER['REQUEST_URI']]);
